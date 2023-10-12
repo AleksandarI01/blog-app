7c4dbd603ea758cd4efd014fbe8aac76f0c17ca0
Aleksandar Ilic <aleks.i6@hotmail.com>
2023-10-12 12:23:31 +0200

    Dark mode theme with Context API created

--- src/components/themeToggle/ThemeToggle.jsx
+++ src/components/themeToggle/ThemeToggle.jsx
@@ -1,13 +1,36 @@
+"use client";
+
 import Image from "next/image";
 import styles from "./themeToggle.module.css";
+import { useContext } from "react";
+import { ThemeContext } from "@/context/ThemeContext";
 
 const ThemeToggle = () => {
-  return <div className={styles.container}>
-    <Image src="/moon.png" alt="dark mode icon" width={14} height={14}/>
-    <div className={styles.circle}></div>
+  const { toggle, theme } = useContext(ThemeContext);
 
-    <Image src="/sun.png" alt="light mode icon" width={14} height={14}/>
-  </div>;
+  return (
+    <div
+      className={styles.container}
+      onClick={toggle}
+      style={
+        theme === "dark"
+          ? { backgroundColor: "white" }
+          : { backgroundColor: "#0f172a" }
+      }
+    >
+      <Image src="/moon.png" alt="dark mode icon" width={14} height={14} />
+      <div
+        className={styles.circle}
+        style={
+          theme === "dark"
+            ? { left: 1, backgroundColor: "#0f172a" }
+            : { right: 1, backgroundColor: "white" }
+        }
+      ></div>
+
+      <Image src="/sun.png" alt="light mode icon" width={14} height={14} />
+    </div>
+  );
 };
 
 export default ThemeToggle;
