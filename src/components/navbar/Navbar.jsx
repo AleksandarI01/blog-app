ab0ba92719cc610f5d2db12ec8ef34bd1584ef5d
Aleksandar Ilic <aleks.i6@hotmail.com>
2023-10-13 13:11:37 +0200

    responsive navbar with burger menu created

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -5,19 +5,27 @@ import AuthLinks from "../authLinks/AuthLinks";
 import ThemeToggle from "../themeToggle/ThemeToggle";
 
 const Navbar = () => {
-  return <div className={styles.container}>
-    <div className={styles.welcome}>
-      <p>Write exciting blogs for others to read</p>
+  return (
+    <div className={styles.container}>
+      <div className={styles.welcome}>
+        <p>Write exciting blogs for others to read</p>
+      </div>
+      <div className={styles.logo}>My Blogify</div>
+      <div className={styles.links}>
+        <ThemeToggle />
+        <Link href="/" className={styles.link}>
+          Homepage
+        </Link>
+        <Link href="/" className={styles.link}>
+          Contact
+        </Link>
+        <Link href="/" className={styles.link}>
+          About
+        </Link>
+        <AuthLinks />
+      </div>
     </div>
-    <div className={styles.logo}>My Blogify</div>
-    <div className={styles.links}>
-      <ThemeToggle />
-      <Link href="/">Homepage</Link>
-      <Link href="/">Contact</Link>
-      <Link href="/">About</Link>
-      <AuthLinks />
-    </div>
-  </div>;
+  );
 };
 
-export default Navbar; 
+export default Navbar;
