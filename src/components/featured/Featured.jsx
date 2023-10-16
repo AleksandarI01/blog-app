e799ada4580b1d2308c53c3808459ad3cf629405
Aleksandar Ilic <aleks.i6@hotmail.com>
2023-10-16 15:55:49 +0200

    design of featured post component created

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,8 +1,35 @@
 import React from "react";
 import styles from "./featured.module.css";
+import Image from "next/image";
 
 const Featured = () => {
-  return <div className={styles.container}>Featured</div>;
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>
+        <b>Hey curious reader!</b> Discover my stories.
+      </h1>
+      <div className={styles.post}>
+        <div className={styles.imgContainer}>
+          <Image
+            src="/featuredImg1.jpg"
+            alt="Image of featured post"
+            fill
+            className={styles.image}
+          />
+        </div>
+        <div className={styles.textContainer}>
+          <h1 className={styles.postTitle}>
+            Lorem ipsum dolor sit amet, coua.
+          </h1>
+          <p className={styles.postDesc}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut la aliqua.
+          </p>
+          <button className={styles.button}>Read more</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Featured;
